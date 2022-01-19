93d873f384aff99190dd29bf22fbb69541ff8f8f
Norman Monteiro <montein56@hotmail.com>
2022-01-18 23:25:01 -0500

    Changed variable names to meaningful camelCase names

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 //GET COLOR INPUT FROM BUTTONS CLICKED AND USER PROMPT FOR SIZE
-function getSize(input){
-  let size=0;
-  while (isNaN(size) || size === null || size < 16 || size > 100){
-    size = prompt ("ETCH-A-SKETCH: Enter the grid size from 16 to 100", 16);
+function getSize(colorSelected){
+  let gridSize=0;
+  while (isNaN(gridSize) || gridSize === null || gridSize < 16 || gridSize > 100){
+    gridSize = prompt ("ETCH-A-SKETCH: Enter the grid gridSize from 16 to 100", 16);
   }
-  drawGrid(size, input);
+  drawGrid(gridSize, colorSelected);
 }
 
 //FUNC FOR DRAWING THE ACTUAL GRID BY CREATING DIVS
-function drawGrid(size, clr) {
-  let n = size, i=-1, j=0, s='';
-  while(++i<n) {
-    s+= '<div class="row">';
-    for(j=0; j<n; j++) s+= `<div class="cell"></div>`;
-    s+= '</div>';
+function drawGrid(gridSize, colorSelected) {
+  let numOfRows=-1, numOfCols=0, divText='';
+  while(++numOfRows<gridSize) {
+    divText+= '<div class="row">';
+    for(numOfCols=0; numOfCols<gridSize; numOfCols++) divText+= `<div class="cell"></div>`;
+    divText+= '</div>';
   }
-  container.innerHTML = s;
+  container.innerHTML = divText;
+   
   const cells = document.getElementsByClassName("cell");
   for (let x = 0; x < cells.length; x++) {
-    cells[x].style["width"] = `${500/n}px`;
-    cells[x].style["height"] = `${500/n}px`;
+    cells[x].style["width"] = `${500/gridSize}px`;
+    cells[x].style["height"] = `${500/gridSize}px`;
     cells[x].addEventListener("mouseenter", event =>        
-    cells[x].style.backgroundColor=rndColor(clr), false); 
+    cells[x].style.backgroundColor=rndColor(colorSelected), false); 
   }
 }
 
 //DEFAULT WHITE COLOR GENERATOR
-let m=280.5;
-function rndColor(xyz){
-  if (xyz=='white'){
+let rgbValue=280.5;
+function rndColor(colorSelected){
+  if (colorSelected=='white'){
     return 'rgb(255, 255, 255)';
     }
 
   //FADE TO BLACK GENERATOR
-  else if (xyz=='fade'){
-    m = m-25.5;
-    if(m<=0){
-      m=255;
+  else if (colorSelected=='fade'){
+    rgbValue = rgbValue-25.5;
+    if(rgbValue<=0){
+      rgbValue=255;
     }
-    return `rgb(${m}, ${m}, ${m}`;
+    return `rgb(${rgbValue}, ${rgbValue}, ${rgbValue}`;
 
   //RANDOM COLOR GENERATOR
   } else {    
